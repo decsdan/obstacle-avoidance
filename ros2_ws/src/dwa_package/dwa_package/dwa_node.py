b39ae29ea3011eade2896d712b7264b2574a2424
decsdan <scheiderd@carleton.edu>
2025-11-12 13:35:16 -0600

    synced lidar and odometer messages,
    Made it so the lidar and odometer are pulled as pairs within 0.1 second of each other, so DWA calcs are correct

--- ros2_ws/src/dwa_package/dwa_package/dwa_node.py
+++ ros2_ws/src/dwa_package/dwa_package/dwa_node.py
@@ -4,8 +4,9 @@ from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
+from message_filters import Subscriber, ApproximateTimeSynchronizer
 
-
+#NOTE: For callbacks, we wanna do as little as possible, just save messages and exit, and process the messages in another function, they block all other callbacks when being processed
 
 class DWA(Node):
     """_summary_
@@ -17,13 +18,25 @@ class DWA(Node):
         """ subscribes to lidar (LaserScan) and Odometry, and preps to publish to movement (Twist), then runs nav_loop over and over
         """
         dt = 0.1
+        self.new_data = False
         super().__init__('dynamic_window_approach')        
-        self.lidar = self.create_subscription(LaserScan, '/scan', self.lidar_callback,10) #the 10 here is how many messages it keeps in queue, not sure what a good number would be but demo_node had 10
-        self.odom = self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
+        self.emergencyLidar = self.create_subscription(LaserScan, '/scan', self.emergency_lidar_callback,10) #the 10 here is how many messages it keeps in queue, not sure what a good number would be but demo_node had 10
+        #self.odom = self.create_subscription(Odometry, '/odom', self.odom_callback, 10) #these are regular subscribers, so things that arent filtered by Approximate TimeSync (we can use these for emergency override stuff, e.g. LIDAR detects an object extremely close but odom hasnt published yet, so we cant trust DWA to stop)
+        self.odom_sub = Subscriber(self, Odometry, '/odom')
+        self.scan_sub = Subscriber(self, LaserScan, '/scan')
         self.twist_publish = self.create_publisher(Twist, '/cmd_vel',10)
-        self.timer = self.create_timer(dt, self.nav_loop)
+        self.timer = self.create_timer(dt, self.nav_loop) #how often navloop runs
+
+
+        self.sync = ApproximateTimeSynchronizer([self.odom_sub, self.scan_sub], 10, 0.1) # 0.1s tolerance
+        self.sync.registerCallback(self.synchronized_callback)
+        self.get_logger().info('Synchronizer node started.')
+
+        self.emergency_scan_msg = None
         self.scan_msg = None
         self.odom_msg = None
+
+
         self.odom_count = 0
         self.scan_count = 0
 
@@ -40,8 +53,8 @@ class DWA(Node):
 
 
 
-    def lidar_callback(self, msg: LaserScan):
-        """gets lidar from the LaserScan
+    def emergency_lidar_callback(self, msg: LaserScan):
+        """gets unsynced raw lidar from the LaserScan directly, for use in emergency stop
         from https://docs.ros.org/en/humble/p/sensor_msgs/
 
 
@@ -76,13 +89,13 @@ class DWA(Node):
                              # device does not provide intensities, please leave
                              # the array empty.
         """        
-        self.scan_msg = msg
+        self.emergency_scan_msg = msg
         self.scan_count += 1
-        self.get_logger().info(f"Recieved scan message {self.scan_count}") #also if you want to see the scan messages, just scan_msg instead of scan_count
+        #self.get_logger().info(f"Recieved scan message {self.scan_count}") #also if you want to see the scan messages, just scan_msg instead of scan_count
 
 
-
-
+    #commented out basic odom_callback, we can decide to use this if we want but for now we have synced messages
+    '''
     def odom_callback(self, msg: Odometry):
         """gets odometry from Odometry
 
@@ -124,13 +137,23 @@ https://docs.ros.org/en/humble/p/nav_msgs/msg/Odometry.html
         
         self.odom_msg = msg
         self.odom_count += 1
-        self.get_logger().info(f"Recieved odom message {self.odom_count}") #also if you want to see the odom messages, just odom_msg instead of odom_count
+        #self.get_logger().info(f"Recieved odom message {self.odom_count}") #also if you want to see the odom messages, just odom_msg instead of odom_count
+
+        '''
+
+    def synchronized_callback(self, odom_msg, scan_msg):
+        self.get_logger().info(f"Synced: Odom time {odom_msg.header.stamp}, Scan time {scan_msg.header.stamp}")
+        self.scan_msg = scan_msg
+        self.odom_msg = odom_msg
+        self.new_data = True
+
+
 
 
     def cost_function(self,trajectory, goal): #TODO implement obstacle avoidance into cost function, not just final state function
 
-        self.get_logger().info(f"TESTING COST FUNCTION")
-
+        #self.get_logger().info(f"TESTING COST FUNCTION")
+        #get final location, twist, direction
         x = trajectory[-1][0]
         y = trajectory[-1][1]
         theta= trajectory[-1][2] #currently this just is a distance from Goal cost function
@@ -145,7 +168,7 @@ https://docs.ros.org/en/humble/p/nav_msgs/msg/Odometry.html
         return score
     
     def predict_trajectory(self,v,w,steps=10, dt=0.1):
-        x, y, theta = 1,2,3 #TODO
+        x, y, theta = 1,2,3 #TODO, need to pull pose from odometry
         trajectory = np.zeros((steps,5))
 
         for i in range(steps):
@@ -181,8 +204,8 @@ https://docs.ros.org/en/humble/p/nav_msgs/msg/Odometry.html
         """   
 
         self.get_logger().info(f"Running NAV LOOP")
-        if self.scan_msg is None or self.odom_msg is None:
-            return     
+        if self.new_data is False: #only run through nav loop fully if weve actually gotten new data, or any data
+            return  
         #TODO implement proper vars here, were gonna have to reference odom_msg pose and twist, im pretty sure to reference twist we have to do odom_msg.twist.twist something something
 
 
@@ -210,7 +233,7 @@ https://docs.ros.org/en/humble/p/nav_msgs/msg/Odometry.html
         #TODO command robot to twist based on best v best w, and publish
 
 
-
+        self.new_data =  False 
 
 
 
